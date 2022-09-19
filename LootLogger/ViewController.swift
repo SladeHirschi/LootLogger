9b2a6fe9f2324e07d34223157d9133f24e3c9dca
Slade Hirschi <sladehirsc@gmail.com>
2022-09-19 13:35:56 -0600

    Initial Commit

---
+++ LootLogger/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LootLogger
+//
+//  Created by Slade Hirschi on 9/19/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
